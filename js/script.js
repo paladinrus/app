7f1296c229a722040dafd40329b02d2da4f96cbb
Pawel <xakrus@bk.ru>
2022-10-24 11:21:41 +0300

    GoTo

--- js/script.js
+++ js/script.js
@@ -10,12 +10,34 @@ const personalMovieDB = {
     privat: false
 };
 
-const a = prompt("Один из последних просомтренных фильмов?", ""),
-      b = prompt("На сколько вы оцениваете его?", ""),
-      c = prompt("Один из последних просомтренных фильмов?", ""),
-      d = prompt("На сколько вы оцениваете его?", "");
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+for (let i = 0; i <2; i++) {
+    const a = prompt("Один из последних просомтренных фильмов?", ""),
+          b = prompt("На сколько вы оцениваете его?", "");
+
+    if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+        personalMovieDB.movies[a] = b;
+        console.log('Done');
+    } else {
+        console.log('Error');
+        i--;
+    }
+
+    
+}
+
+
+if (personalMovieDB.count < 10) {
+    console.log('Просомтрено довольно мало фильмов');
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+    console.log('Вы классический зритель');
+} else if (personalMovieDB.count <= 30) {
+    console.log('Вы киноман');
+} else {
+    console.log('Произошла ошибка');
+}
+
+
+
 
 console.log(personalMovieDB);
